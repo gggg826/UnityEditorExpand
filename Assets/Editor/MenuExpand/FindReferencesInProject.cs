b7e6e0914d13edc2ec2f7cff838c80718e1683cd
gggg826 <liushida87@126.com>
2017-07-28 22:39:26 +0800

    Add FindReferencesInProect

---
+++ Assets/Editor/MenuExpand/FindReferencesInProject.cs
@@ -0,0 +1,66 @@
+﻿/********************************************************************
+*	created:	28/7/2017   22:13
+*	filename: 	FindReferences
+*	author:		Bing Lau
+*	
+*	purpose:	https://github.com/gggg826/UnityEditorExpand
+*********************************************************************/
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class FindReferencesInProject : Editor
+{
+
+    [MenuItem("Assets/Find References In Project", false, 25)]
+    static private void Find()
+    {
+        EditorSettings.serializationMode = SerializationMode.ForceText;
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (!string.IsNullOrEmpty(path))
+        {
+            string guid = AssetDatabase.AssetPathToGUID(path);
+            List<string> withoutExtensions = new List<string>() { ".prefab", ".unity", ".mat", ".asset" };
+            string[] files = Directory.GetFiles(Application.dataPath, "*.*", SearchOption.AllDirectories)
+                .Where(s => withoutExtensions.Contains(Path.GetExtension(s).ToLower())).ToArray();
+            int startIndex = 0;
+
+            EditorApplication.update = delegate ()
+            {
+                string file = files[startIndex];
+
+                bool isCancel = EditorUtility.DisplayCancelableProgressBar("匹配资源中", file, (float)startIndex / (float)files.Length);
+
+                if (Regex.IsMatch(File.ReadAllText(file), guid))
+                {
+                    Debug.Log(file, AssetDatabase.LoadAssetAtPath<Object>(GetRelativeAssetsPath(file)));
+                }
+
+                startIndex++;
+                if (isCancel || startIndex >= files.Length)
+                {
+                    EditorUtility.ClearProgressBar();
+                    EditorApplication.update = null;
+                    startIndex = 0;
+                    Debug.Log("匹配结束");
+                }
+
+            };
+        }
+    }
+
+    [MenuItem("Assets/Find References In Project", true, 25)]
+    static private bool EnableMenu()
+    {
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        return string.IsNullOrEmpty(path) ? false : File.Exists(path) ? true : false;
+    }
+
+    static private string GetRelativeAssetsPath(string path)
+    {
+        return "Assets" + Path.GetFullPath(path).Replace(Path.GetFullPath(Application.dataPath), "").Replace('\\', '/');
+    }
+}
