0eb1abaf5896a0da8043826869a233fccf8e3da0
gggg826 <liushida@126.com>
2017-07-31 21:23:06 +0800

    Add StartGame Button

--- Assets/Editor/ViewExpand/SceneViewExpand.cs
+++ Assets/Editor/ViewExpand/SceneViewExpand.cs
@@ -14,31 +14,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneViewExpand
 {
-    private static SceneViewExpand m_Instance;
-    public static SceneViewExpand Instance
-    {
-        get
-        {
-            if (null == m_Instance)
-                m_Instance = new SceneViewExpand();
-            return m_Instance;
-        }
-    }
-    public int MaxRecordScenesLength = 5;
+	private static SceneViewExpand m_Instance;
+	public static SceneViewExpand Instance
+	{
+		get
+		{
+			if (null == m_Instance)
+				m_Instance = new SceneViewExpand();
+			return m_Instance;
+		}
+	}
+	public int MaxRecordScenesLength = 5;
 
-    private List<EditorViewItem> m_SceneExpandItems;
-    private GUIContent[] SceneDisplayOptions;
-    private string[] m_RecordScenes;
-    private string m_RegKey_RecordCount;
-    private string m_RegKey_RecordPrefix;
-    private string m_LastScene;
+	private List<EditorViewItem> m_SceneExpandItems;
+	private GUIContent[] SceneDisplayOptions;
+	private string[] m_RecordScenes;
+	private string m_RegKey_RecordCount;
+	private string m_RegKey_RecordPrefix;
+	private string m_LastScene;
 	private bool m_Started;
-    public SceneViewExpand()
-    {
-        string projectName = Path.GetFileNameWithoutExtension(System.Environment.CurrentDirectory);
-        m_RegKey_RecordCount = projectName + "_Scenes_Count";
-        m_RegKey_RecordPrefix = projectName + "_ScenesRecord_";
-        m_LastScene = SceneManager.GetActiveScene().path;
+	public SceneViewExpand()
+	{
+		string projectName = Path.GetFileNameWithoutExtension(System.Environment.CurrentDirectory);
+		m_RegKey_RecordCount = projectName + "_Scenes_Count";
+		m_RegKey_RecordPrefix = projectName + "_ScenesRecord_";
+		m_LastScene = SceneManager.GetActiveScene().path;
 		m_Started = false;
 
 		int length = EditorPrefs.GetInt(m_RegKey_RecordCount, 0);
@@ -49,61 +49,64 @@ public class SceneViewExpand
 		}
 		UpdateSceneDisplayOptions();
 		m_SceneExpandItems = new List<EditorViewItem>();
-        ViewExpandUtils.AddCustom(ref m_SceneExpandItems, AddRecentButton);
-    }
+		AddRecentButton();
+		AddSpace();
+		AddStartGameButton();
+		AddSpace();
+	}
 
-    public void OnHierarchyWindowChanged()
-    {
-        if (m_LastScene != SceneManager.GetActiveScene().path || !m_Started)
-        {
+	public void OnHierarchyWindowChanged()
+	{
+		if (m_LastScene != SceneManager.GetActiveScene().path || !m_Started)
+		{
 			m_Started = true;
-            m_LastScene = SceneManager.GetActiveScene().path;
-            UpdateRecordScenes(SceneManager.GetActiveScene().path);
+			m_LastScene = SceneManager.GetActiveScene().path;
+			UpdateRecordScenes(SceneManager.GetActiveScene().path);
 			SaveConfig();
 		}
-    }
+	}
 
-    public void OnSceneFunc(SceneView sceneView)
-    {
-        Handles.BeginGUI();
-        GUILayout.Space(-18);
-        GUILayout.BeginHorizontal(EditorStyles.toolbar, GUILayout.ExpandWidth(true));
-        foreach (var item in m_SceneExpandItems)
-            item.Draw();
-        GUILayout.EndHorizontal();
-        Handles.EndGUI();
-    }
-    
-    /// <summary>
-    /// 关闭旧场景，打开新场景
-    /// </summary>
-    /// <param name="userData"></param>
-    /// <param name="options"></param>
-    /// <param name="selected"></param>
-    private void OnRecentMenuSelected(object userData, string[] options, int selected)
-    {
-        if (selected >= 0 && selected < m_RecordScenes.Length)
-        {
-            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
-            {
-                EditorSceneManager.OpenScene(m_RecordScenes[selected]);
-            }
-        }
-    }
-    
-    private void UpdateRecordScenes(string scenePath)
-    {
-        List<string> recordScenes = new List<string>(m_RecordScenes);
-        if (File.Exists(scenePath) && !string.IsNullOrEmpty(scenePath))
-        {
-            if (recordScenes.Contains(scenePath))
-                recordScenes.Remove(scenePath);
-            recordScenes.Insert(0, scenePath);
-            if (recordScenes.Count > MaxRecordScenesLength)
-                recordScenes.RemoveRange(MaxRecordScenesLength, recordScenes.Count - MaxRecordScenesLength);
-        }
+	public void OnSceneFunc(SceneView sceneView)
+	{
+		Handles.BeginGUI();
+		GUILayout.Space(-18);
+		GUILayout.BeginHorizontal(EditorStyles.toolbar, GUILayout.ExpandWidth(true));
+		foreach (var item in m_SceneExpandItems)
+			item.Draw();
+		GUILayout.EndHorizontal();
+		Handles.EndGUI();
+	}
 
-        m_RecordScenes = recordScenes.ToArray();
+	/// <summary>
+	/// 关闭旧场景，打开新场景
+	/// </summary>
+	/// <param name="userData"></param>
+	/// <param name="options"></param>
+	/// <param name="selected"></param>
+	private void OnRecentMenuSelected(object userData, string[] options, int selected)
+	{
+		if (selected >= 0 && selected < m_RecordScenes.Length)
+		{
+			if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+			{
+				EditorSceneManager.OpenScene(m_RecordScenes[selected]);
+			}
+		}
+	}
+
+	private void UpdateRecordScenes(string scenePath)
+	{
+		List<string> recordScenes = new List<string>(m_RecordScenes);
+		if (File.Exists(scenePath) && !string.IsNullOrEmpty(scenePath))
+		{
+			if (recordScenes.Contains(scenePath))
+				recordScenes.Remove(scenePath);
+			recordScenes.Insert(0, scenePath);
+			if (recordScenes.Count > MaxRecordScenesLength)
+				recordScenes.RemoveRange(MaxRecordScenesLength, recordScenes.Count - MaxRecordScenesLength);
+		}
+
+		m_RecordScenes = recordScenes.ToArray();
 		UpdateSceneDisplayOptions();
 
 	}
@@ -116,7 +119,7 @@ public class SceneViewExpand
 			SceneDisplayOptions[i] = new GUIContent(i + " " + Path.GetFileNameWithoutExtension(m_RecordScenes[i]));
 		}
 	}
-    
+
 	private void SaveConfig()
 	{
 		EditorPrefs.SetInt(m_RegKey_RecordCount, m_RecordScenes.Length);
@@ -126,14 +129,53 @@ public class SceneViewExpand
 		}
 	}
 
-    private void AddRecentButton()
-    {
-        GUIContent contentRecent = new GUIContent("Recent");
-        GUIStyle styleRecent = EditorStyles.toolbarDropDown;
-        Rect rect = GUILayoutUtility.GetRect(contentRecent, styleRecent, GUILayout.Width(60));
-        if (GUI.Button(rect, contentRecent, styleRecent))
-        {
-            EditorUtility.DisplayCustomMenu(rect, SceneDisplayOptions, -1, OnRecentMenuSelected, null);
-        }
-    }
+	/// <summary>
+	/// 添加Recent Scene下拉菜单
+	/// </summary>
+	private void AddRecentButton()
+	{
+		ViewExpandUtils.AddCustom(ref m_SceneExpandItems
+								 , () =>
+								 {
+									 GUIContent contentRecent = new GUIContent("Recent");
+									 GUIStyle styleRecent = EditorStyles.toolbarDropDown;
+									 Rect rect = GUILayoutUtility.GetRect(contentRecent, styleRecent, GUILayout.Width(60));
+									 if (GUI.Button(rect, contentRecent, styleRecent))
+									 {
+										 EditorUtility.DisplayCustomMenu(rect, SceneDisplayOptions, -1, OnRecentMenuSelected, null);
+									 }
+								 });
+	}
+
+	/// <summary>
+	/// 添加启动游戏菜单（无论当前是哪个场景只要BuildSettings里添加过场景就会自动切换到第0个场景并启动游戏）
+	/// </summary>
+	private void AddStartGameButton()
+	{
+		string buttonName = "StartGame";
+		string tooltip = "不管你现在在编辑哪个Scene，只要点击这个按钮就可以直接从Login运行游戏";
+		ViewExpandUtils.AddPushButton(ref m_SceneExpandItems
+									, buttonName
+									, tooltip
+									, () =>
+									{
+										if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+										{
+											EditorBuildSettingsScene[] sceneInBuilderSettings = EditorBuildSettings.scenes;
+											if(sceneInBuilderSettings.Length > 0 && !string.IsNullOrEmpty(sceneInBuilderSettings[0].path))
+												EditorSceneManager.OpenScene(sceneInBuilderSettings[0].path);
+											EditorApplication.isPlaying = true;
+										}
+									}, GUILayout.Width(70));
+	}
+
+	private void AddUIEditorButton()
+	{
+
+	}
+
+	private void AddSpace()
+	{
+		ViewExpandUtils.AddSpace(ref m_SceneExpandItems, 6);
+	}
 }
