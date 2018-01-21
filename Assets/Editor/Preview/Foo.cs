dbdc82b19c08ded7198b3abc122751c4b1e301d9
gggg826 <liushida@126.com>
2018-01-21 22:58:58 +0800

    Preview

---
+++ Assets/Editor/Preview/Foo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditorInternal;
+public class Foo : EditorWindow
+{
+	Mesh mPreviewMesh;
+	Material mPreviewMaterial;
+	PreviewRenderUtility mPreviewRenderUtility;
+	[MenuItem("Tools/Foo")]
+	static void Setup()
+	{
+		GetWindow(typeof(Foo));
+	}
+	void OnGUI()
+	{
+		if (mPreviewRenderUtility == null)
+		{
+			mPreviewRenderUtility = new PreviewRenderUtility();
+			mPreviewRenderUtility.m_Camera.farClipPlane = 500;
+			mPreviewRenderUtility.m_Camera.clearFlags = CameraClearFlags.SolidColor;
+			mPreviewRenderUtility.m_Camera.transform.position = new Vector3(0, 0, -10);
+			var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			var meshFilter = go.GetComponent<MeshFilter>();
+			mPreviewMesh = meshFilter.sharedMesh;
+			mPreviewMaterial = go.GetComponent<Renderer>().sharedMaterial;
+			DestroyImmediate(go);
+		}
+		var drawRect = new Rect(0, 0, 100, 100);
+		mPreviewRenderUtility.BeginPreview(drawRect, GUIStyle.none);
+		InternalEditorUtility.SetCustomLighting(mPreviewRenderUtility.m_Light, new Color(0.6f, 0.6f, 0.6f, 1f));
+		mPreviewRenderUtility.DrawMesh(mPreviewMesh, Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(30, 45, 0), Vector3.one), mPreviewMaterial, 0);
+		mPreviewRenderUtility.m_Camera.Render(); var texture = mPreviewRenderUtility.EndPreview();
+		InternalEditorUtility.RemoveCustomLighting();
+		GUI.Box(drawRect, texture);
+	}
+}
---
+++ Assets/Editor/Preview/PreviewExampleWindow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PreviewExampleWindow : EditorWindow
+{
+	private Editor m_Editor;
+
+	[MenuItem("Window/PreviewExample")]
+	static void ShowWindow()
+	{
+		GetWindow<PreviewExampleWindow>("PreviewExample");
+	}
+
+	private void OnDestroy()
+	{
+		if (m_Editor != null)
+		{
+			DestroyImmediate(m_Editor);
+		}
+		m_Editor = null;
+	}
+
+	void OnGUI()
+	{
+		if (m_Editor == null)
+		{
+			// 第一个参数这里暂时没关系，因为编辑器没有取目标对象
+			m_Editor = Editor.CreateEditor(this, typeof(PreviewExampleInspector));
+		}
+
+		m_Editor.DrawPreview(GUILayoutUtility.GetRect(300, 200));
+	}
+}
